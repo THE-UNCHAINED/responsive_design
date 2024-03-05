55756e1321ecc9c0ed4e8a4bcc07f0cbdd8e9a1c
Prashant <108423066+THE-UNCHAINED@users.noreply.github.com>
2024-03-05 22:30:08 +0530

    responsive design

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:ressponsive_learning/const/constant.dart';
+import 'package:ressponsive_learning/screens/main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dashborad UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: backgroundColor,
+        brightness: Brightness.dark,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
---
+++ lib/widgets/dashboard_widget.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+import 'package:ressponsive_learning/util/responsive.dart';
+import 'package:ressponsive_learning/widgets/activity_details_card.dart';
+import 'package:ressponsive_learning/widgets/bar_graph_widget.dart';
+import 'package:ressponsive_learning/widgets/header_widget.dart';
+import 'package:ressponsive_learning/widgets/line_chart_card.dart';
+import 'package:ressponsive_learning/widgets/summary_widget.dart';
+
+class DashboardWidget extends StatelessWidget {
+  const DashboardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 18),
+        child: Column(
+          children: [
+            const SizedBox(height: 18),
+            const HeaderWidget(),
+            const SizedBox(height: 18),
+            const ActivityDetailsCard(),
+            const SizedBox(height: 18),
+            const LineChartCard(),
+            const SizedBox(height: 18),
+            const BarGraphCard(),
+            const SizedBox(height: 18),
+            if (Responsive.isTablet(context)) const SummaryWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+}
